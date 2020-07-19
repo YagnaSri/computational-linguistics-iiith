cb0fd0d902254ee46af87e14de33f9e280ad3b53
YagnaSri <yagnadalawai10@gmail.com>
2020-07-19 22:08:12 +0530

    G10

--- RDIP/Codes/sentences.js
+++ RDIP/Codes/sentences.js
@@ -1,28 +1,17 @@
 var lanenglish = [
-["John ate an apple before afternoon<br>","before afternoon John ate an apple<br>", "John before afternoon ate an apple<br>"],
- ["some students like to study in the night<br>", "at night some students like to study<br>"],
-["John and Mary went to church<br>","Mary and John went to church<br>"],
-["John went to church after eating<br>","after eating John went to church<br>","John after eating went to church<br>"],
-["did he go to market<br>","he did go to market<br>"],
-["the woman who called my sister sells cosmetics<br>","the woman who sells cosmetics called my sister<br>","my sister who sells cosmetics called the woman<br>","my sister who called the woman sells cosmetics<br>"],
-["John goes to the library and studies<br>","John studies and goes to the library<br>"],
-["John ate an apple so did she<br>","she ate an apple so did John<br>"],
-["the teacher returned the book after she noticed the error","the teacher noticed the error after she returned the book<br>","after the teacher returned the book she noticed the error<br>","after the teacher noticed the error she returned the book<br>","she returned the book after the teacher noticed the error<br>","she noticed the error after the teacher returned the book<br>","after she returned the book the teacher noticed the error<br>","after she noticed the error the teacher returned the book<br>"],
-["I told her that I bought a book yesterday<br>","I told her yesterday that I bought a book<br>","yesterday I told her that I bought a book<br>","I bought a book that I told her yesterday<br>","I bought a book yesterday that I told her<br>","yesterday I bought a book that I told her<br>"]
+["John ate an apple before afternoon","before afternoon John ate an apple", "John before afternoon ate an apple"],
+ ["some students like to study in the night", "at night some students like to study"],
+["John and Mary went to church","Mary and John went to church"],
+["John went to church after eating","after eating John went to church","John after eating went to church"],
+["did he go to market","he did go to market"],
+["the woman who called my sister sells cosmetics","the woman who sells cosmetics called my sister","my sister who sells cosmetics called the woman","my sister who called the woman sells cosmetics"],
+["John goes to the library and studies","John studies and goes to the library"],
+["John ate an apple so did she","she ate an apple so did John"],
+["the teacher returned the book after she noticed the error","the teacher noticed the error after she returned the book","after the teacher returned the book she noticed the error","after the teacher noticed the error she returned the book","she returned the book after the teacher noticed the error","she noticed the error after the teacher returned the book","after she returned the book the teacher noticed the error","after she noticed the error the teacher returned the book"],
+["I told her that I bought a book yesterday","I told her yesterday that I bought a book","yesterday I told her that I bought a book","I bought a book that I told her yesterday","I bought a book yesterday that I told her","yesterday I bought a book that I told her"]
 ];
 
 
-var allEng= [         "John ate an apple before afternoon",
-                      "some students like to study in the night",
-                      "John and Mary went to church",
-                      "John went to church after eating",
-                      "did he go to market",
-                      "the woman who called my sister sells cosmetics",
-                       "John goes to the library and studies",
-                        "John ate an apple so did she",
-                     "the teacher returned the book after she noticed the error",
-                     "I bought a book yesterday that I told her",
-            ]
 
 var lanhindi=[
 ["राम और श्याम बाजार गयें",
@@ -73,46 +62,37 @@ var lanhindi=[
 "है वहाँ बड़ी सी एक किताब"]
 ]
 
-var allhindi=["राम और श्याम बाजार गयें",
-                       "राम सोया और श्याम भी",
-                       "मैंने उसे बताया कि राम सो रहा है",
-                       "राम खाकर सोया",
-                       "बिल्लियों को मारकर कुत्ता सो गया",
-                       "एक लाल किताब वहाँ है",
-                       "एक बड़ी सी किताब वहाँ है"
-]
+
+
 
 var array= ["राम और श्याम बाजार गयें","राम और श्याम गयें बाजार","बाजार गयें राम और श्याम","गयें बाजार राम और श्याम","राम सोया और श्याम भी","श्याम सोया और राम भी","सोया श्याम और राम भी","सोया राम और श्याम भी","मैंने उसे बताया कि राम सो रहा है","मैंने उसे बताया कि सो रहा है राम","उसे मैंने बताया कि राम सो रहा है","उसे मैंने बताया कि सो रहा है राम","मैंने बताया उसे कि राम सो रहा है","मैंने बताया उसे कि सो रहा है राम","उसे बताया मैंने कि राम सो रहा है","उसे बताया मैंने कि सो रहा है राम","बताया मैंने उसे कि राम सो रहा है","बताया मैंने उसे कि सो रहा है राम","बताया उसे मैंने कि राम सो रहा है","बताया उसे मैंने कि सो रहा है राम","राम खाकर सोया","खाकर राम सोया","राम सोया खाकर","खाकर सोया राम","सोया राम खाकर","सोया खाकर राम","बिल्लियों को मारकर कुत्ता सो गया","मारकर बिल्लियों को कुत्ता सो गया","बिल्लियों को मारकर सो गया कुत्ता","मारकर बिल्लियों को सो गया कुत्ता","कुत्ता सो गया बिल्लियों को मारकर","कुत्ता सो गया मारकर बिल्लियों को","सो गया कुत्ता बिल्लियों को मारकर","सो गया कुत्ता मारकर बिल्लियों को","एक लाल किताब वहाँ है","एक लाल किताब है वहाँ","वहाँ है एक लाल किताब","है वहाँ एक लाल किताब","एक बड़ी सी किताब वहाँ है","एक बड़ी सी किताब है वहाँ","बड़ी सी एक किताब वहाँ है","बड़ी सी एक किताब है वहाँ","वहाँ है एक बड़ी सी किताब","वहाँ है बड़ी सी एक किताब","है वहाँ एक बड़ी सी किताब","है वहाँ बड़ी सी एक किताब","John ate an apple before afternoon","before afternoon John ate an apple","John before afternoon ate an apple","some students like to study in the night","at night some students like to study","John and Mary went to church","Mary and John went to church","John went to church after eating","after eating John went to church","John after eating went to church","did he go to market","he did go to market","the woman who called my sister sells cosmetics","the woman who sells cosmetics called my sister","my sister who sells cosmetics called the woman","my sister who called the woman sells cosmetics", "John goes to the library and studies","John studies and goes to the library", "John ate an apple so did she","she ate an apple so did John", "the teacher returned the book after she noticed the error","the teacher noticed the error after she returned the book","after the teacher returned the book she noticed the error","after the teacher noticed the error she returned the book","she returned the book after the teacher noticed the error","she noticed the error after the teacher returned the book","after she returned the book the teacher noticed the error","after she noticed the error the teacher returned the book", "I told her that I bought a book yesterday","I told her yesterday that I bought a book","yesterday I told her that I bought a book","I bought a book that I told her yesterday","I bought a book yesterday that I told her","yesterday I bought a book that I told her" ];
 
+  y = [Math.floor(Math.random()*lanenglish.length)];
+ y =[Math.floor(Math.random()*lanhindi.length)];
 
-
-
-function display()
-
+function display() 
 {
-  clear();
-	var lang = document.getElementById("select_lang").value;
-
-	//console.log(lang);
-  
-	if(lang == "Hindi")
+   clear();
+	if(document.getElementById("select_lang").value=='Hindi')
       {
-    
-         document.getElementById("dis1").innerHTML="Form a sentence (Declarative or Interrogative or any other type) from the given          words";
+         document.getElementById("show").innerHTML="";
+         document.getElementById("dis1").innerHTML="Form a sentence (Declarative or Interrogative or any other type) from the given words";
          document.getElementById("dis2").innerHTML="(select the buttons in proper order)";
-         var  h = allhindi[Math.floor(Math.random()*allhindi.length)];
-         var s_s=h.split(" ");
+//console.log(y);
+        t = lanhindi[y][0];
+         var s_s=t.split(" ");
          jums_s = jumble(s_s);
 	 jumbtn(jums_s);
     }
-    else if(lang == "English")
+    else if(document.getElementById("select_lang").value=='English')
 {
         document.getElementById("dis1").innerHTML="Form a sentence (Declarative or Interrogative or any other type) from the given words";
         document.getElementById("dis2").innerHTML="(select the buttons in proper order)";
 
-
-         var  e = allEng[Math.floor(Math.random()*allEng.length)];
-         var s_s=e.split(" ");
+ document.getElementById("show").innerHTML="";
+        
+        t=lanenglish[y][0];
+         var s_s=t.split(" ");
 	 jums_s = jumble(s_s);
 	 jumbtn(jums_s);
         console.log(s_s);
@@ -122,26 +102,25 @@ function display()
 }
 
 
-	function jumble(array)
-	{
-		
-                l=array.length;
-		console.log(l);
-        for(var i=0;i<l;i++)
+function jumble(array)
+{
+ l=array.length;
+   for(var i=0;i<l;i++)
          {
               var  ord =Math.floor(Math.random()*l);
-                 e=array[i];
+                
+                 var temp=array[i];
                 array[i]=array[ord];
-               array[ord]=e;
-         }
-		return array;
+               array[ord]=temp;
+         }	
+return array;
 	}
-	function jumbtn(array)
+
+function jumbtn(array)
 	{
 		var count=0;
 		var l = array.length;
                 var btn=0;
-
         for(var h =0 ;h<l;h++)
         {
                 
@@ -159,16 +138,15 @@ document.getElementById("reform").innerHTML="<button  id='reform' onclick='reset
                this.style.display="none";
                btn++;
     sentence=document.getElementById("dissent").innerHTML;
-                                 console.log(sentence);
-			//console.log(btn);
+                               //  console.log(sentence);
+			
                         //console.log(jums_s.length);
 			if(btn == jums_s.length)
 				document.getElementById("check").innerHTML="<button  onclick='check()'>Check the correctnes of this sentence</button>";
 });
-}
 
          }
-
+}
 
 function check()
 {
@@ -184,11 +162,47 @@ function check()
                      }
             }
              document.getElementById('crt').innerHTML= "Wrong Answer";
-             document.getElementById("showbtn").innerHTML="<button >Get Correct Sentence</button>";
+             document.getElementById('showbtn').innerHTML="<button onclick='showing()'>Get Correct Sentence</button>";
+}
+
+function showing()
+{
+         ans="";
+         num =0;
+ if(document.getElementById("select_lang").value=='English')
+          {
+          // console.log(lanenglish[y]);
+           num = lanenglish[y].length-1;
+      document.getElementById("showbtn").innerHTML="<button onclick='hide()' >Hide correct answer</button>";
+             for(let k=0;k<=num;k++)
+               {
+                               ans = ans+"<center>"+lanenglish[y][k]+"<br></center>";
+                 }
+ document.getElementById("show").innerHTML = ans;
+            }
+ else if(document.getElementById("select_lang").value=='Hindi')
+{
+           num = lanhindi[y].length-1;
+      document.getElementById("showbtn").innerHTML="<button onclick='hide()' >Hide correct answer</button>";
+             for(let k=0;k<=num;k++)
+{
+                               ans = ans+"<center>"+lanhindi[y][k]+"<br></center>";
+}
+document.getElementById("show").innerHTML = ans;
+}
 }
 
 
-
+function hide(){
+document.getElementById("showbtn").innerHTML="<button onclick='toggle()'> Get Correct Sentence</button>"
+document.getElementById("show").innerHTML="";
+}
+ 
+function toggle(){
+while(document.getElementById("show").innerHTML == ""){
+document.getElementById('showbtn').innerHTML ="<button onclick='hide()'>hide correct answer</button>"
+}
+}
 
 
 
@@ -197,8 +211,9 @@ function reset()
 	clear();
         document.getElementById("demo1").innerHTML="";
 	jumbtn(jums_s);  
-             document.getElementById("crt").innerHTML="";
 }
+
+
 function clear()
 {    
 	btn=0;
@@ -208,4 +223,6 @@ function clear()
 	document.getElementById("reform").innerHTML="";
                    document.getElementById("check").innerHTML="";             
                    document.getElementById("crt").innerHTML="";
+                    document.getElementById("showbtn").innerHTML="";
+                    document.getElementById("show").innerHTML=""
 }
